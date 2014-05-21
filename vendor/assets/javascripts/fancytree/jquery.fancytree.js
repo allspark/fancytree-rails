7d4da74c343f51d38efb580edab0effd76e46738
Dennis Boerm <allspark@planetcyborg.de>
2014-05-21 12:45:32 +0200

    generate form: idPrefix

--- vendor/assets/javascripts/fancytree/jquery.fancytree.js
+++ vendor/assets/javascripts/fancytree/jquery.fancytree.js
@@ -1,4 +1,4 @@
-/*!
+
  * jquery.fancytree.js
  * Dynamic tree view control, with support for lazy loading of branches.
  * https://github.com/mar10/fancytree/
@@ -1863,8 +1863,8 @@ Fancytree.prototype = /** @lends Fancytree# */{
 	generateFormElements: function(selected, active) {
 		// TODO: test case
 		var nodeList,
-			selectedName = (selected !== false) ? "ft_" + this._id : selected,
-			activeName = (active !== false) ? "ft_" + this._id + "_active" : active,
+			selectedName = (selected !== false) ? options.idPrefix + this._id + "[]" : selected,
+			activeName = (active !== false) ? options.idPrefix + this._id + "_active" : active,
 			id = "fancytree_result_" + this._id,
 			$result = this.$container.find("div#" + id);
 
